294f5d64c99a9845b7871ba9e6ef1801fbc22519
Molly Huerster <mollyhuerster@squareup.com>
2022-12-02 14:22:03 -0500

    Day 1 Part 2 (#2)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,27 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun getElvesFromInput(input: List<String>): List<List<Int>> {
         val groupSeparator = ",\\s,\\s".toRegex()
         val itemSeparator = ",\\s+".toRegex()
 
         val groups = input.joinToString()
         val elves = groups.split(groupSeparator).map { it.split(itemSeparator).map { it.toInt() } }//
+        return(elves)
+    }
+
+    fun part1(input: List<String>): Int {
+        val elves = getElvesFromInput(input)
         val calories = elves.map { it.sum() }
         val result = calories.max()
 
-        println(result)
         return(result)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = getElvesFromInput(input)
+        val calories = elves.map { it.sum() }
+        return(calories.sortedDescending().take(3).sum())
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_sample")
     val input = readInput("Day01")
     check(part1(testInput) == 24000)
-    part1(input)
+    check(part2(testInput) == 45000)
 
-    //println(part1(input))
-    //println(part2(input))
+    println(part1(input))
+    println(part2(input))
 }
