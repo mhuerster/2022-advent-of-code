cb6cdd7be7d68610601e23600e916fe57d7f1642
Molly Huerster <mollyhuerster@squareup.com>
2022-12-05 18:03:31 -0500

    Day 3 Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -27,20 +27,30 @@ fun main() {
     }!!.toChar()
   }
 
+  fun badge(rucksacks: List<String>): Char {
+    val (firstElf, secondElf, thirdElf) = rucksacks.map { it.toSet() }
+    return(firstElf.intersect(secondElf).intersect(thirdElf)).single()
+  }
+
   fun part1(rucksacks: List<String>): Int {
     return rucksacks.fold(0) { sum, rucksack ->
       sum + priority((commonItem(rucksack)))
     }
   }
 
-  fun part2(input: List<String>): Int {
-    return input.size
+  fun part2(rucksacks: List<String>): Int {
+    val groups = rucksacks.chunked(3)
+
+    return groups.fold(0) { sum, group ->
+      sum + priority(badge(group))
+    }
   }
 
   // test if implementation meets criteria from the description, like:
   val testInput = readInput("Day03_sample")
   println(part1(testInput))
   check(part1(testInput) == 157)
+  check(part2(testInput) == 70)
 
   val input = readInput("Day03")
   println(part1(input))
